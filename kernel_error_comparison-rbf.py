f8c97995d2de5197bbc93aec3a14a2d1ff5f5184
Joshua Zahner <jaz101@login4.pegasus.edu>
2018-07-11 14:29:21 -0400

    Updates to kernel comparison scripts

--- kernel_error_comparison-rbf.py
+++ kernel_error_comparison-rbf.py
@@ -8,11 +8,6 @@ from kernels import CurlFreeKernel as cfk, DivFreeKernel as dfk
 def run_models(samples):
     trajectory = Trajectory(nsamples=samples, integration_time=30, n_timesteps=15, pattern=Pattern.random)
 
-    div_k = dfk.DivFreeK(3)
-    curl_k = cfk.CurlFreeK(3)
-
-    k = div_k + curl_k
-
     regression = reg.Regression(dim=3)
     regression.initialize_samples(nsamples=samples, trajectory=trajectory)
 
@@ -20,31 +15,24 @@ def run_models(samples):
 
     rbf_e = regression.compute_error(errors=["ge_av_raw"])
 
-    regression.run_model(kernel=k)
-
-    cdk_e = regression.compute_error(errors=["ge_av_raw"])
-
-    return rbf_e, cdk_e
+    return rbf_e
 
 def main():
     rbf_errors = []
-    cdk_errors = []
 
     sample = 1
-    while sample <= 70:
+    while sample <= 300:
 
         print(sample)
 
         try:
-            rbf_e, cdk_e = run_models(samples=sample)
+            rbf_e = run_models(samples=sample)
 
             rbf_errors.append(rbf_e[0])
-            cdk_errors.append(cdk_e[0])
 
             sample = sample + 20
 
             np.savetxt("rbf_errors.csv", rbf_errors)
-            np.savetxt("cdf_errors.csv", cdk_errors)
 
         except Exception as e:
             print("An error ocurred")
