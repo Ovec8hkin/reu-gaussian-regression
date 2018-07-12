24aff6b9dda0a6c714ccbdb4efccbab729b81b24
Joshua Zahner <joshuazahner@gmail.com>
2018-07-12 12:07:05 -0400

    Setup sample_density plot code

---
+++ samples_density_plot.py
@@ -0,0 +1,53 @@
+import numpy as np
+import regression as reg
+from trajectories.Trajectory import Trajectory
+import matplotlib.pylab as pl
+from mpl_toolkits.mplot3d import Axes3D
+
+def import_array(file):
+    return np.loadtxt(file, dtype=np.float64, delimiter=",")
+
+def f(S, D):
+
+    zs = np.zeros(shape=(S.size, D.size))
+
+    for i in range(len(S)):
+        for j in range(len(D)):
+
+            s = S[i][j]
+            d = D[i][j]
+
+            regression = reg.Regression(dim=3)
+
+            trajectory = Trajectory(nsamples=s, integration_time=30, n_timesteps=10, density=d)
+
+            regression.initialize_samples(1, trajectory=trajectory)
+            regression.run_model()
+
+            print("{}, {}".format(s, d))
+
+            e = regression.compute_error(errors=['ge_av_raw'])
+
+            print(e)
+
+            zs[i][j] = e[0]
+
+    return zs
+
+
+if __name__ == "__main__":
+
+    s = np.arange(1, 181, 20)
+    d = np.arange(0.01, 2.26, 0.25)
+
+    S, D = np.meshgrid(s, d)
+
+    z = import_array("/Users/Desktop/sample_density.csv")
+
+    fig = pl.figure()
+    ax = pl.axes(projection='3d')
+
+    ax.plot_surface(S, D, z, rstride=1, cstride=1,
+                 cmap='viridis', edgecolor='none')
+
+    pl.show()
