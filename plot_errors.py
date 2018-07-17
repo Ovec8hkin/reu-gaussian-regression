52f70dc36ff622aebb4c4c468a854ae295e2c952
Joshua Zahner <joshuazahner@gmail.com>
2018-07-17 18:34:04 -0400

    Generalized plot_errors routine you use directory rather than reference to each file individually

--- plot_errors.py
+++ plot_errors.py
@@ -4,30 +4,42 @@ from timeseries_regression import TimeseriesRegression
 from trajectories.Trajectory import Trajectory, Pattern
 
 
-def main(ur_file, vr_file):
+def main(directory, drifters):
 
-    ur = np.loadtxt(ur_file, delimiter=",")
-    vr = np.loadtxt(vr_file, delimiter=",")
+    ur_file, vr_file, ku_file, kv_file = None, None, None, None
+
+    try:
+        ur_file = directory+str(drifters)+"_drifters_ur.csv"
+        vr_file = directory + str(drifters) + "_drifters_vr.csv"
+        ku_file = directory+str(drifters)+"_drifters_ku.csv"
+        kv_file = directory + str(drifters) + "_drifters_kv.csv"
+    except Exception as e:
+        print("File not found")
+
+    ur = np.loadtxt(ur_file, delimiter=",") if ur_file is not None else None
+    vr = np.loadtxt(vr_file, delimiter=",") if vr_file is not None else None
+
+    #ku = np.loadtxt(ku_file, delimiter=",") if ku_file is not None else None
+    #kv = np.loadtxt(kv_file, delimiter=",") if kv_file is not None else None
 
     reg = TimeseriesRegression()
 
-    trajectory = Trajectory(nsamples=30, integration_time=360, n_timesteps=48, pattern=Pattern.grid)
+    trajectory = Trajectory(nsamples=drifters, integration_time=3600, n_timesteps=48, pattern=Pattern.grid)
     reg.initialize_samples(trajectory=trajectory)
 
     reg.ur = ur
     reg.vr = vr
+    #reg.ku = ku
+    #reg.kv = kv
 
     reg.plot_errors()
 
 
 if __name__ == "__main__":
 
-    # main(
-    #     ur_file="/Users/joshua/Desktop/gpr-drifters/model_output/300_samples_ur.csv",
-    #     vr_file="/Users/joshua/Desktop/gpr-drifters/model_output/300_samples_vr.csv",
-    # )
+    main("/Users/joshua/Desktop/gpr-drifters/model_output/", 300)
 
-    main(
-        ur_file="/Users/joshua/Desktop/gpr-drifters/ur_save.csv",
-        vr_file="/Users/joshua/Desktop/gpr-drifters/vr_save.csv"
-    )
+    # main(
+    #     ur_file="/Users/joshua/Desktop/gpr-drifters/ur_save.csv",
+    #     vr_file="/Users/joshua/Desktop/gpr-drifters/vr_save.csv"
+    # )
